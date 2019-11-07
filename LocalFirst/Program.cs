b77465d38472f626404db46d8849c1937eb1c605
你爹 <14422750@qq.com>
2019-11-07 16:19:21 +0800

    Signed-off-by: 哦耶 <sunchengpeng@hotmail.com>

--- LocalFirst/Program.cs
+++ LocalFirst/Program.cs
@@ -22,7 +22,7 @@ namespace LocalFirst
 
             Console.WriteLine("Hello Chris");
             Console.WriteLine("Hello Chris2");
-            Console.WriteLine("Hello Chris688");
+            Console.WriteLine("Hello Chris688000");
             
 
         }
