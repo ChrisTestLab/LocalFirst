86b7727164646a8931c8fab277c4532b24e80ead
Chris@hotmail <sunchengpeng@hotmail.com>
2019-11-07 15:07:06 +0800

    12

--- LocalFirst/Program.cs
+++ LocalFirst/Program.cs
@@ -17,6 +17,7 @@ namespace LocalFirst
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Hello Chris mail q");
         }
     }
 }
