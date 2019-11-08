53402a6718cf0ee91a353a9bbe3b8eaee8f0858a
你爹 <sunchengpeng@hotmail.com>
2019-11-08 16:14:20 +0800

    34

--- LocalFirst/Program.cs
+++ LocalFirst/Program.cs
@@ -22,7 +22,7 @@ namespace LocalFirst
 
             Console.WriteLine("Hello Chris");
             Console.WriteLine("Hello Chris2");
-            Console.WriteLine("Hello Chris666");
+            Console.WriteLine("Hello Chris99900");
             
 
         }
